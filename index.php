200c3fc3f0b0c841abe19ecc4bdda6781810bc7a
florianazemi <flori-777@hotmail.com>
2022-07-16 18:03:10 +0200

    Added Header, Navigation and Fonts

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="styles.css">
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Florian's Portfolio</title>
+</head>
+<body>
+<div class="header1">
+<h5>Florian Azemi</h5>
+        <div class="container1">
+        
+            <nav>
+                <a href="#">Home</a>
+                <a href="#">About</a>
+                <a href="#">Projects</a>
+                <a href="#" class="buttoni">Contact</a>
+            </nav>
+        </div>
+    </div>
+    <div class="container">
+        <div class="container-child">
+        <p class="sm-p">Hello, my name is <b>Florian</b><br>
+        I'm a Website Developer and Designer based in Kosovo.</p>
+        
+        </div>
+        <div class="container-child2">
+        <img src="images/imgprofile.jpg" alt="" style="
+    width: 400px;">
+        </div>
+        
+    </div>    
+    <div class="footer1">
+        <div class="container">Copyright &copy; 2022 Florian Azemi</div>        
+    </div>
+    
+</body>
+</html>
