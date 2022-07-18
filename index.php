49093c4c6d5e79b61a779cce13d84c607f0bdf3c
florianazemi <flori-777@hotmail.com>
2022-07-18 19:06:12 +0200

    Finished Projects Section

--- index.php
+++ index.php
@@ -1,69 +1,113 @@
+<? $servername = "localhost";
+   $username = "root";
+   $password = "";
+    
+   $conn = mysqli_connect($servername,
+                  $username, $password);
+    
+   
+   if (!$conn) {
+     die("Connection failed: "
+         . mysqli_connect_error());
+   }
+   echo "Connected successfully";
+   ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="styles.css">
-
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Florian's Portfolio</title>
-</head>
-<body>
-<div class="header1">
-<h5>Florian Azemi</h5>
-        <div class="container1">
-        
+   <head>
+      <meta charset="UTF-8">
+      <link rel="stylesheet" href="styles.css">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Florian's Portfolio</title>
+   </head>
+   <body>
+      <div class="header1">
+         <h5>Florian Azemi</h5>
+         <div class="container1">
             <nav>
-                <a href="#">Home</a>
-                <a href="#about">About</a>
-                <a href="#skills">Skills</a>
-                <a href="#">Projects</a>
-                <a href="#" class="buttoni">Contact</a>
+               <a href="#">Home</a>
+               <a href="#about">About</a>
+               <a href="#skills">Skills</a>
+               <a href="#">Projects</a>
+               <a href="#" class="buttoni">Contact</a>
             </nav>
-        </div>
-    </div>
-    <div class="container">
-        <div class="container-child">
-        <p class="sm-p">Hello, my name is <b>Florian</b><br>
-        I'm a Website Developer and Designer based in Kosovo.</p>
-        
-        </div>
-        <div class="container-child2">
-        <img src="imgprofile.jpg" alt="" style="
-    width: 400px;">
-        </div>
-        
-        <div id="about" class="container-child4">
-        <p class="bg-p">About Me</p>
-        </div>
-
-        <div class="container-child3">
-        <p class="sm-p">My background in Web-Design and eye for design perfectly compliments my passion for software development with a focus in efficient and componentized UI development.</p>
-        </div>
-
-        <div id="skills" class="container-child6">
-        <p class="bg-p">My Skills</p>
-        </div>
-        <div class="container-child5">
+         </div>
+      </div>
+      <div class="container">
+         <div class="container-child">
+            <p class="sm-p">Hello, my name is <b>Florian</b><br>
+               I'm a Website Developer and Designer based in Kosovo.
+            </p>
+         </div>
+         <div class="container-child2">
+            <img src="imgprofile.jpg" alt="" style="
+               width: 400px;">
+         </div>
+         <div id="about" class="container-child4">
+            <p class="bg-p">About Me</p>
+         </div>
+         <div class="container-child3">
+            <p class="sm-p">My background in Web-Design and eye for design perfectly compliments my passion for software development with a focus in efficient and componentized UI development.</p>
+         </div>
+         <div id="skills" class="container-child6">
+            <p class="bg-p">My Skills</p>
+         </div>
+         <div class="container-child5">
             <?php 
-            $dir = "skills/*.png";
-            $images = glob( $dir );
-            foreach( $images as $image ):
-                echo "
-                <div class='card'>
-                <div class='card-image'>
-                    <img src='" . $image . "' alt='" . $image . "'>
-                </div>
-            </div>";
-            endforeach;
+               $dir = "skills/*.png";
+               $images = glob( $dir );
+               foreach( $images as $image ):
+                   echo "
+                   <div class='card'>
+                   <div class='card-image'>
+                       <img src='" . $image . "' alt='" . $image . "'>
+                   </div>
+               </div>";
+               endforeach;
+               ?>
+         </div>
+         <div id="projects" class="container-child6">
+            <p class="bg-p">Projects</p>
+         </div>
+         <div class="container-child5">
+
+         <?php
+               include_once 'config.php';
+               $row = mysqli_query($conn,"SELECT * FROM projects;");
+               ?>
+               
+               <?php
+               if(!empty($row))
+               foreach($row as $rows)
+              { 
+                
             ?>
-            
-        </div>
+            <div class='project-card'>
+                <div class='project-card-image'>
+                <img src="<?php echo $rows['image']; ?>" alt="">
+               </div>
+               <div class='project-card-title'>
+                <p><?php echo $rows['title']; ?></p>
+               </div>
+               
+               <div class='project-card-language'>
+                <p>Made with - <b> <?php echo $rows['p_language']; ?></b></p>
+               </div>
+               <div class='project-card-date'>
+                <p><?php echo $rows['year']; ?></p>
+               </div>
+            </div>
         
-    </div>    
-    <div class="footer1">
-        <div class="container">Copyright &copy; 2022 Florian Azemi</div>        
-    </div>
-    
-</body>
+                
+              
+            <?php } ?>
+            
+            
+         </div>
+      </div>
+      <div class="footer1">
+         <div class="container">Copyright &copy; 2022 Florian Azemi</div>
+      </div>
+   </body>
 </html>
